d2a94fd93a7e3c27c83a6bf8bf6dedafa955f3b3
Zach M <zatchmdws@gmail.com>
2020-04-23 10:36:57 -0400

    Fixed the thread cards not working and got descriptions to show up

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./styles.css";
 
-function ProjectCard() {
+function ProjectCard(props) {
+
+  
   return (
     <div className="projectCard animated fadeIn ">
-      
+      <img class ="cardImage" src= {props.image} alt="Project" />
+      <p className="projectCardBody">{props.description}</p>
     </div>
   );
 }
+
 
 export default ProjectCard;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import ProjectCard from "../components/ProjectCard";
-
-
+import Cadenza from "../assets/images/cadenzaimage.png";
 
 class Projects extends Component {
-    render() {
-        return (
-            <div>
-                <ProjectCard />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ProjectCard
+          image="https://zachmdws.github.io/Portfolio/Assets/Images/workimage.png"
+          description="I constructed a day planner that would allow a busy employee to schedule their day on a normal 9 to 5 business hour basis. Feel free to check out more here!"
+        />
+        <ProjectCard 
+          image="https://zachmdws.github.io/Portfolio/Assets/Images/fitimage.png"
+          description="A collaborative effort between myself and 3 other programmers. We created a fitness application that would use answers given by the user to produce a workout."
+        />
+        <ProjectCard
+          image={Cadenza}
+          description="A final project assignment when I completed my course at the UNCC Coding Bootcamp. A forum website designed and created by myself and 3 other programmers."
+        />
+      </div>
+    );
+  }
 }
 export default Projects;
