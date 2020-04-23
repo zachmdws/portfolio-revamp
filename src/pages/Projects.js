de1b446d9302ea546c60c1493439622579cef456
Zach M <zatchmdws@gmail.com>
2020-04-23 10:38:55 -0400

    Fixed images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import ProjectCard from "../components/ProjectCard";
+import Planner from "../assets/images/workimage.png";
+import Fit from "../assets/images/fitimage.png";
 import Cadenza from "../assets/images/cadenzaimage.png";
 
 class Projects extends Component {
@@ -7,11 +9,11 @@ class Projects extends Component {
     return (
       <div>
         <ProjectCard
-          image="https://zachmdws.github.io/Portfolio/Assets/Images/workimage.png"
+          image={Planner}
           description="I constructed a day planner that would allow a busy employee to schedule their day on a normal 9 to 5 business hour basis. Feel free to check out more here!"
         />
         <ProjectCard 
-          image="https://zachmdws.github.io/Portfolio/Assets/Images/fitimage.png"
+          image={Fit}
           description="A collaborative effort between myself and 3 other programmers. We created a fitness application that would use answers given by the user to produce a workout."
         />
         <ProjectCard
