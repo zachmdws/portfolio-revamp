aff192434a9e8415909e8b809c26ec25310d9567
Zach M <zatchmdws@gmail.com>
2020-04-25 20:51:41 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from './logo.svg';
 import Navbar from "./components/Navbar";
 import Projects from "./pages/Projects";
 import NameLogo from "./components/NameLogo";
-import Wrapper from "./components/Wrapper";
 import About from "./pages/About";
-import Footer from "./components/Footer";
 import './App.css';
 
 function App() {
@@ -15,11 +12,8 @@ function App() {
       <div>
         <NameLogo />
         <Navbar />
-        {/* <Wrapper> */}
           <Route exact path="/" component={About}/>
           <Route exact path="/personal-projects" component={Projects}/>
-        {/* </Wrapper> */}
-        {/* <Footer /> */}
       </div>
     </Router>
     
