b57bac203c835a76132f5b947538f9f0a87a6e82
Zach M <zatchmdws@gmail.com>
2022-05-05 18:50:38 -0400

    Ayyyyy

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Projects from "./pages/Projects";
-import NameLogo from "./components/NameLogo";
-import About from "./pages/About";
+import Navbar from "../components/Navbar";
+import Projects from "../pages/Projects";
+import NameLogo from "../components/NameLogo";
+import About from "../pages/About";
 import '../App.css';
 
 function App() {
