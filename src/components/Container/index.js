810dd45dd78784b1aca89dff28f22c1f6f5a4b5e
Zach M <zatchmdws@gmail.com>
2022-05-05 16:11:46 -0400

    GGG

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 function Container(props) { 
-    <video autoPlay loop muted id= 'video'>
-        <source url = "https://www.youtube.com/watch?v=G52dUQLxPzg" type='video/mp4'/>
+    <video autoPlay loop muted id= 'video'> <source url = "https://www.youtube.com/watch?v=G52dUQLxPzg" type='video/mp4'/>
     </video>
-    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;}
+    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
+}
 
 export default Container;
