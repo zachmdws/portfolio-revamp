60a7b9e6f3ff1b732d8b1dbaea04a30cfd9c8de2
Zach M <zatchmdws@gmail.com>
2022-05-05 15:48:18 -0400

    Video background test

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Container(props) { 
+    <video autoPlay loop muted id= 'video'>
+        <source src="https://www.youtube.com/watch?v=G52dUQLxPzg" type='video/mp4'/>
+    </video>
     return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
 
