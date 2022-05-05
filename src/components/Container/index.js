b669bda6311d5fb93b5fd3a41e03ba7b936260a5
Zach M <zatchmdws@gmail.com>
2022-05-05 16:09:18 -0400

    GGG

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,7 +4,6 @@ function Container(props) {
     <video autoPlay loop muted id= 'video'>
         <source url = "https://www.youtube.com/watch?v=G52dUQLxPzg" type='video/mp4'/>
     </video>
-    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
-}
+    return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;}
 
 export default Container;
