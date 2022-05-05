c07775883c2dc9537a6eb314a87ad083a2d03d00
Zach M <zatchmdws@gmail.com>
2022-05-05 16:13:49 -0400

    GGG

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 function Container(props) { 
-    <video autoPlay loop muted id= 'video'> <source url = "https://www.youtube.com/watch?v=G52dUQLxPzg" type='video/mp4'/>
-    </video>
+
     return <div className={`container${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
 
