01e1f23b2071eb5ae46f4fc830cec50292b01156
Zach M <zatchmdws@gmail.com>
2022-05-06 11:42:00 -0400

    Working on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
             <a class = "lemmesee"href="https://github.com/zachmdws" >
                 <img className ="footerImg" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="Github Logo" style={{height: "55px", borderRadius: "50%", marginRight: "15px"}}/>
             </a>
-            <a class = "lemmesee" href="https://www.linkedin.com/in/zachary-meadows-102681145/" >
+            <a href="https://www.linkedin.com/in/zachary-meadows-102681145/" >
                 <img className ="footerImg" src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" alt="LinkedIn Logo" style={{height: "55px"}}/>
             </a>
             
