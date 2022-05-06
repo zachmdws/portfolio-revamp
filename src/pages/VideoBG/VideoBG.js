8d87d50c13c0130fa87ba591011dd3fa833816a4
Zach M <zatchmdws@gmail.com>
2022-05-06 11:08:59 -0400

    JJJ

--- src/pages/VideoBG/VideoBG.js
+++ src/pages/VideoBG/VideoBG.js
@@ -1,7 +1,7 @@
 import React from "react";
 import VideoBg from "reactjs-videobg";
-import mp4 from "/Users/zachb/HTML/portfolio-revamp/src/ocean-video-background.mp4";
-import poster from "/Users/zachb/HTML/LB-World/LBsWorld/public/assets/images/nyan-cat-LB.gif"
+import mp4 from "/Users/zachb/HTML/portfolio-revamp/src/assets/images/ocean-video-background.mp4";
+//import poster from "/Users/zachb/HTML/LB-World/LBsWorld/public/assets/images/nyan-cat-LB.gif"
 
 function VideoBG(){ 
   return (
