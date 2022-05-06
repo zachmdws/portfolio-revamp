ca87c9bf52b6b1dd92ce4f385abe6f057af50923
Zach M <zatchmdws@gmail.com>
2022-05-06 07:19:30 -0400

    Incubus

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -11,8 +11,7 @@ function AboutMe() {
         of projects utilizing technologies such as MySQL and MongoDB. I am also
         accustom to JavaScript itself and React.
         
-        Feel free to check out my Github page: <a class="link"href="https://github.com/zachmdws">https://github.com/zachmdws</a>
-        As well as my LinkedIn: <a class="link"href="https://www.linkedin.com/in/zachary-meadows-102681145/">https://www.linkedin.com/in/zachary-meadows-102681145/</a>
+        Feel free to check out my Github page as well as my LinkedIn, below. 
       </p>
     )
 }
