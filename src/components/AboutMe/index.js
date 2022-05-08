a855af5de0b309f1556bfdd5f7e382cc37e33072
Zach M <zatchmdws@gmail.com>
2022-05-08 19:30:55 -0400

    Fixed grammatical errors.

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,14 +4,14 @@ import "./style.css";
 function AboutMe() {
     return ( 
         <p className="about animated fadeIn delay-.5s">
-        I am Zachary Meadows a graduate of the University of North Carolina at
+        I am Zachary Meadows, a graduate of the University of North Carolina at
         Charlotte with a Bachelor's of Science in Accounting. I recently
         completed The Coding Boot Camp at UNC Charlotte and am an aspiring
         junior web developer! My strengths include working on the database end
         of projects utilizing technologies such as MySQL and MongoDB. I am also
         accustom to JavaScript itself and React.
         
-        Feel free to check out my Github page as well as my LinkedIn, 
+        Feel free to check out my Github page as well as my LinkedIn.
       </p>
     )
 }
