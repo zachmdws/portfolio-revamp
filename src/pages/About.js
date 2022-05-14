889adb04dc1c9749c06c2e791f38bd65264a1526
Zach M <zatchmdws@gmail.com>
2022-05-14 16:22:44 -0400

    Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,27 +21,8 @@ class About extends Component {
 
 
             <div>
-          <Container style={{ marginTop: 30 }}>
-        <a 
-        href="https://github.com/zachmdws/Day-Planner"
-        target="_blank"
-        rel="noopener noreferrer">
-
-        <ProjectCard
-          image={Planner}
-          description="I constructed a day planner that would allow a busy employee to schedule their day on a normal 9 to 5 business hour basis. Feel free to check out more by clicking one of the boxes!"
-        />
-        </a>
-        <a 
-        href="https://github.com/zachmdws/Fitness-Tracker"
-        target="_blank"
-        rel="noopener noreferrer">
-
-        <ProjectCard 
-          image={Fit}
-          description="A collaborative effort between myself and 3 other programmers. We created a fitness application that would use answers given by the user to produce a workout."
-        />
-        </a>
+          <Container style={{ marginTop: 30 }}> 
+          
         <a 
         href="https://github.com/MHargett23/Music-Forum"
         target="_blank"
