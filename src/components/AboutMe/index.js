e3cc87b995e4ef8a9226f54fcc1ea053820cebce
Zach M <zatchmdws@gmail.com>
2022-05-16 08:24:37 -0400

    Fixing more CSS stuff

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,12 +4,10 @@ import "./style.css";
 function AboutMe() {
     return ( 
         <p className="about animated fadeIn delay-.5s">
-        I am a graduate of the University of North Carolina at
-        Charlotte with a Bachelor's of Science in Accounting. I recently
-        completed The Coding Boot Camp at UNC Charlotte and am an aspiring
-        junior web developer! My strengths include working on the database end
-        of projects utilizing technologies such as MySQL and MongoDB. I am also
-        accustom to JavaScript itself and React.
+        I am a junior web developer at the beginning of my coding
+        career. JavaScript, React, Node, HTML, CSS, MYSQL and MongoDB are just
+        some of the coding languages and programs I am becoming more and more accustomed
+        to utilizing.
         
         Feel free to check out my Github page as well as my LinkedIn.
       </p>
